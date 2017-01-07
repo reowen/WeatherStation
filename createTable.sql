2d0481e3092f1ddd1af7790f977584bcdc9dd8fa
Russell <russ.e.owen@gmail.com>
2017-01-07 11:36:25 -0500

    Update datetime in create table script

--- createTable.sql
+++ createTable.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `bmp180` (
   `barometric_pressure` varchar(20) NOT NULL,
   `altitude` varchar(20) NOT NULL,
   `dateMeasured` date NOT NULL,
-  `hourMeasured` date NOT NULL,
+  `hourMeasured` datetime NOT NULL,
   `timeBlock` int(128) NOT NULL,
   PRIMARY KEY (`reading_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1  ;
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS `dht22` (
   `temperature_f` double NOT NULL,
   `humidity` varchar(20) NOT NULL,
   `dateMeasured` date NOT NULL,
-  `hourMeasured` date NOT NULL,
+  `hourMeasured` datetime NOT NULL,
   `timeBlock` int(128) NOT NULL,
   PRIMARY KEY (`dht_id`),
   FOREIGN KEY (`reading_id`) REFERENCES bmp180(`reading_id`)
