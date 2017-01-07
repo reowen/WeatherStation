8f384a04dcd3fac3de0d9d2c7b6532b9b79451e1
Russell <russ.e.owen@gmail.com>
2017-01-07 11:33:27 -0500

    Type

--- createTable.sql
+++ createTable.sql
@@ -5,18 +5,19 @@ CREATE TABLE IF NOT EXISTS `bmp180` (
   `barometric_pressure` varchar(20) NOT NULL,
   `altitude` varchar(20) NOT NULL,
   `dateMeasured` date NOT NULL,
-  `hourMeasured` int(128) NOT NULL,
+  `hourMeasured` date NOT NULL,
   `timeBlock` int(128) NOT NULL,
   PRIMARY KEY (`reading_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1  ;
 
 CREATE TABLE IF NOT EXISTS `dht22` (
   `dht_id` int(255) NOT NULL AUTO_INCREMENT,
+  `reading_id` int(20) NOT NULL,
   `temperature_c` double NOT NULL,
   `temperature_f` double NOT NULL,
   `humidity` varchar(20) NOT NULL,
   `dateMeasured` date NOT NULL,
-  `hourMeasured` int(128) NOT NULL,
+  `hourMeasured` date NOT NULL,
   `timeBlock` int(128) NOT NULL,
   PRIMARY KEY (`dht_id`),
   FOREIGN KEY (`reading_id`) REFERENCES bmp180(`reading_id`)
